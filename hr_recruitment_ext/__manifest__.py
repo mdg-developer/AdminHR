4ac087a9dae0c25c13571ccc97e930ea1778301c
Lin Latt <devuser@7thcomputing.com>
2023-02-24 10:37:37 +0630

    MDGHR - 192 Recruitment အောက် က By Job Position မှာ List View နဲ့ ပြပေးပါ။

---
+++ hr_recruitment_ext/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hr Recruitment Extended',
+    'version': '1.0.0',
+    'author': '7thcomputing',
+    'license': 'AGPL-3',
+    'category': 'Leave',
+    'website': 'http://7thcomputing.com',
+    'description': """
+
+Public Holidays
+    """,
+    'depends': ['base','hr'],
+    'data': [
+        'views/hr_views_job.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
